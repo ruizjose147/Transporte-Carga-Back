ee35ea2a6e3587e9f888f17ca424a42ae6aa389e
ruizjose147 <ruizjose_147@hotmail.com>
2022-04-23 16:13:00 -0500

    Se agregan servicios para la entidad Vehiculo

--- src/main/java/com/sofka/app/transportecargaback/vehiculo/service/VehiculoService.java
+++ src/main/java/com/sofka/app/transportecargaback/vehiculo/service/VehiculoService.java
@@ -1,9 +1,28 @@
 package com.sofka.app.transportecargaback.vehiculo.service;
 
+import com.sofka.app.transportecargaback.vehiculo.collection.Vehiculo;
+import com.sofka.app.transportecargaback.vehiculo.repositorie.VehiculoRepositorieI;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Service
 public class VehiculoService {
 
+    @Autowired
+    VehiculoRepositorieI repositorieI;
+
+    public Mono<Vehiculo> save(Vehiculo vehiculo){
+        return this.repositorieI.save(vehiculo);
+    }
+
+    public Flux<Vehiculo> findAll(){
+        return this.repositorieI.findAll();
+    }
+
+    public Mono<Vehiculo> findByPlaca(String placa){
+        return this.repositorieI.findByPlaca(placa);
+    }
 
 }
