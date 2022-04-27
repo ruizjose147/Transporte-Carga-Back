41881d45d73ba8ec49cbbe41019339f9e2ff44fd
mariiana-lp <mariana.lopez.docs_221@gmail.com>
2022-04-27 09:47:58 -0500

    Test servicio findAll, completo

--- src/test/java/com/sofka/app/transportecargaback/vehiculo/service/VehiculoServiceTest.java
+++ src/test/java/com/sofka/app/transportecargaback/vehiculo/service/VehiculoServiceTest.java
@@ -2,16 +2,31 @@ package com.sofka.app.transportecargaback.vehiculo.service;
 
 import com.sofka.app.transportecargaback.Conductor.collection.Conductor;
 import com.sofka.app.transportecargaback.vehiculo.collection.Vehiculo;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
 
-@SpringBootTest
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ExtendWith(SpringExtension.class)
 class VehiculoServiceTest {
 
-    @Autowired VehiculoService service;
+    @Autowired
+    @MockBean
+    VehiculoService service;
+
+    @Autowired
+    private WebTestClient webTestClient;
 
     @Test
     void save() {
@@ -29,26 +44,38 @@ class VehiculoServiceTest {
 
     }
 
-    /*@Test
+    @Test
+    @DisplayName("findAll")
     void findAll() {
         Vehiculo vehiculo = new Vehiculo();
+        Conductor conductor = new Conductor(1001014265, "Carlos", 34, "3216538976", "carlos@gmail.com");
         vehiculo.setPlaca("ABC123");
         vehiculo.setMarca("TOYOTA");
         vehiculo.setAnio(2020);
         vehiculo.setCapacidad(1000);
         vehiculo.setTipo("PICK UP");
+        vehiculo.setConductor(conductor);
         service.save(vehiculo);
 
         Vehiculo vehiculoDos = new Vehiculo();
+        Conductor conductor2 = new Conductor(7625265, "Simon", 24, "3298238976", "carlos@gmail.com");
         vehiculoDos.setPlaca("ABC456");
         vehiculoDos.setMarca("MAZDA");
         vehiculoDos.setAnio(2022);
         vehiculoDos.setCapacidad(800);
         vehiculoDos.setTipo("PICK UP");
+        vehiculo.setConductor(conductor2);
         service.save(vehiculoDos);
 
-        Flux<Vehiculo> todos = service.findAll();
-        StepVerifier.create(todos).expectNext(vehiculo).expectNext(vehiculoDos).verifyComplete();
+        List<Vehiculo> list = new ArrayList<>();
+        list.add(vehiculo);
+        list.add(vehiculoDos);
+
+        Flux<Vehiculo> flux = Flux.fromIterable(list);
+        Mockito.when(service.findAll()).thenReturn(flux);
+        //when(mock.findAll()).thenReturn(flux);
+        webTestClient.get().uri("/vehiculos").exchange().expectStatus();
+
 
     }
 
@@ -65,5 +92,5 @@ class VehiculoServiceTest {
 
         Flux<Vehiculo> marca = service.findByMarca("TOYOTA");
         StepVerifier.create(marca).expectNext(vehiculo).verifyComplete();
-    }*/
+    }
 }
